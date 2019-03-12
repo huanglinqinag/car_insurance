e78f99f835899c6c9dabaf9c4af732cfdfcd55da
DongYu <1948919174@qq.com>
2019-03-12 15:23:44 +0800

    第一次

---
+++ src/test/java/com/uhb/car/carApplicationTests.java
@@ -0,0 +1,45 @@
+package com.uhb.car;
+
+
+import com.uhb.car.CarApplication;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {CarApplication.class})
+public class carApplicationTests {
+
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    //MockMvcRequestBuilders.get("/user").contentType(MediaType.APPLICATION_JSON_UTF8))
+    //                .andExpect(MockMvcResultMatchers.status().isOk()
+    @Test
+    public void whenQuerySuccess() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/Test")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(3))
+        ;
+    }
+}
+
